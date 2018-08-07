e9decce6369452dc18215387368bbcc082f9e53a
okandavut <davut.okan@gmail.com>
2018-08-07 10:18:11 +0300

    Api call was added and calling bisim api now

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,246 +3,74 @@ import "bulma/css/bulma.css";
 import "./Home.css";
 
 class Home extends Component {
-  render() {
-    return (
-      <div>
-        <div class="columns">
-          <div class="column is-one-fifth">
-            <div class="card">
-              <div class="card-image" />
-              <div class="card-content">
-                <div class="media">
-                  <div class="media-left">
-                    <figure class="image-aktif is-48x48">
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+  componentDidMount() {
+    this.interval = setInterval(() =>
+      fetch("https://api.citybik.es/v2/networks/baksi-bisim")
+        .then(res => res.json())
+        .then(
+          (result) => {
+            this.setState({
+              isLoaded: true,
+              items: result.network
+            });
+          },
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error
+            });
+          }
+        ), 10000
 
-                    </figure>
-                  </div>
-                  <div class="media-content">
-                    <p class="title is-4">Üçkuyular</p>
-                    <p class="subtitle is-6">İskele-Bisim</p>
-                  </div>
-                </div>
-                <div class="content">
-                  Toplam Bisiklet : 20<br />
-                  Boştaki Bisiklet : 10
-                  <br />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="column is-one-fifth">
-            <div class="card">
-              <div class="card-image" />
-              <div class="card-content">
-                <div class="media">
-                  <div class="media-left">
-                    <figure class="image-aktif is-48x48">
-                      
-                    </figure>
-                  </div>
-                  <div class="media-content">
-                    <p class="title is-4">Karşıyaka</p>
-                    <p class="subtitle is-6">İskele-Bisim</p>
-                  </div>
-                </div>
-
-                <div class="content">
-                  Toplam Bisiklet : 15<br />
-                  Boştaki Bisiklet : 5
-                  <br />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="column is-one-fifth">
-            <div class="card">
-              <div class="card-image" />
-              <div class="card-content">
-                  <div class="media">
-                  <div class="media-left">
-                    <figure class="image-pasif is-48x48">
-                    </figure>
-                  </div>
-                  <div class="media-content">
-                    <p class="title is-4">Konak</p>
-                    <p class="subtitle is-6">İskele-Bisim</p>
-                  </div>
-                </div>
-
-                <div class="content">
-                  Toplam Bisiklet : 5<br />
-                  Boştaki Bisiklet : 5
-                  <br />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="column is-one-fifth">
-            <div class="card">
-              <div class="card-image" />
-              <div class="card-content">
-                   <div class="media">
-                  <div class="media-left">
-                    <figure class="image-aktif is-48x48">
-                    </figure>
-                  </div>
-                  <div class="media-content">
-                    <p class="title is-4">Sasalı</p>
-                    <p class="subtitle is-6">İskele-Bisim</p>
-                  </div>
-                </div>
-
-                <div class="content">
-                  Toplam Bisiklet : 12<br />
-                  Boştaki Bisiklet : 0
-                  <br />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="column is-one-fifth">
-            <div class="card">
-              <div class="card-image" />
-              <div class="card-content">
-                  <div class="media">
-                  <div class="media-left">
-                    <figure class="image-pasif is-48x48">
-                    </figure>
-                  </div>
-                  <div class="media-content">
-                    <p class="title is-4">Bayraklı</p>
-                    <p class="subtitle is-6">İskele-Bisim</p>
-                  </div>
-                </div>
-
-                <div class="content">
-                  Toplam Bisiklet : 15<br />
-                  Boştaki Bisiklet : 3
-                  <br />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="columns second">
-          <div class="column is-one-fifth">
-            <div class="card">
-              <div class="card-image" />
-              <div class="card-content">
-                 <div class="media">
-                  <div class="media-left">
-                    <figure class="image-pasif is-48x48">
-                    </figure>
-                  </div>
-                  <div class="media-content">
-                    <p class="title is-4">Alsancak</p>
-                    <p class="subtitle is-6">İskele-Bisim</p>
-                  </div>
-                </div>
-
-                <div class="content">
-                  Toplam Bisiklet : 30<br />
-                  Boştaki Bisiklet : 30
-                  <br />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="column is-one-fifth">
-            <div class="card">
-              <div class="card-image" />
-              <div class="card-content">
-                   <div class="media">
-                  <div class="media-left">
-                    <figure class="image-aktif is-48x48">
-                    </figure>
-                  </div>
-                  <div class="media-content">
-                    <p class="title is-4">Alsancak-2</p>
-                    <p class="subtitle is-6">İskele-Bisim</p>
-                  </div>
-                </div>
-
-                <div class="content">
-                  Toplam Bisiklet : 22<br />
-                  Boştaki Bisiklet : 12
-                  <br />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="column is-one-fifth">
-            <div class="card">
-              <div class="card-image" />
-              <div class="card-content">
-                   <div class="media">
-                  <div class="media-left">
-                    <figure class="image-aktif is-48x48">
-                    </figure>
-                  </div>
-                  <div class="media-content">
-                    <p class="title is-4">Mavişehir</p>
-                    <p class="subtitle is-6">İskele-Bisim</p>
-                  </div>
-                </div>
-
-                <div class="content">
-                  Toplam Bisiklet : 3<br />
-                  Boştaki Bisiklet : 3
-                  <br />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="column is-one-fifth">
-            <div class="card">
-              <div class="card-image" />
-              <div class="card-content">
-                   <div class="media">
-                  <div class="media-left">
-                    <figure class="image-pasif is-48x48">
-                    </figure>
-                  </div>
-                  <div class="media-content">
-                    <p class="title is-4">Tersane</p>
-                    <p class="subtitle is-6">İskele-Bisim</p>
-                  </div>
-                </div>
-
-                <div class="content">
-                  Toplam Bisiklet : 4<br />
-                  Boştaki Bisiklet : 2
-                  <br />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="column is-one-fifth">
-            <div class="card">
-              <div class="card-image" />
-              <div class="card-content">
-                   <div class="media">
-                  <div class="media-left">
-                    <figure class="image-aktif is-48x48">
-                    </figure>
-                  </div>
-                  <div class="media-content">
-                    <p class="title is-4">Bostanlı</p>
-                    <p class="subtitle is-6">İskele-Bisim</p>
-                  </div>
-                </div>
-
-                <div class="content">
-                  Toplam Bisiklet : 6<br />
-                  Boştaki Bisiklet : 1
-                  <br />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
     );
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div className="loading">Loading...</div>;
+    } else {
+      return (
+        <div class="columns is-multiline is-mobile">  
+                {items.stations.map(item => (
+          <div className="column is-one-fifth">
+            <div className="card card-css">
+              <div className="card-image" />
+              <div className="card-content">
+                <div className="media"> 
+                  <div className="media-left">
+                    <figure className={item.extra.status == 'Active' ?"image-aktif" : "image-pasif"}>
+                    </figure>
+                  </div>
+                  <div className="media-content">
+                    <p className="title is-4 yazi">{item.name}</p>
+                    <p className="subtitle is-6">Bisim</p>
+                  </div>
+                </div>
+                <div className="content">
+                  Toplam Bisiklet : {item.extra.slots}<br />
+                  Boştaki Bisiklet : {item.empty_slots}
+                  <br />
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+        </div>
+      );
+    }
   }
 }
 
